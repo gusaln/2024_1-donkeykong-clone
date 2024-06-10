251a3ac56485c7577e9ce0f2495b9f27f7a67c17
Gustavo López <git.gustavolopez.xyz@gmail.com>
2024-06-09 21:32:11 -0400

    refactor: Move logic to animator

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,37 +1,63 @@
 using UnityEngine;
+using System;
 
 public class Player : MonoBehaviour
 {
-    private SpriteRenderer spriteRenderer;
-    public Sprite[] runSprites;
-    public Sprite climbSprite;
-    private int spriteIndex;
+    // private SpriteRenderer spriteRenderer;
+    // public Sprite[] runSprites;
+    // public Sprite climbSprite;
+    // private int spriteIndex;
 
     private new Rigidbody2D rigidbody;
     private new Collider2D collider;
 
+    private Animator animator;
+
     private Collider2D[] overlaps = new Collider2D[4];
     private Vector2 direction;
 
-    private bool grounded;
+    private bool _isGrounded;
+    private bool isGrounded {
+         get
+        {
+            return _isGrounded;
+        }
+        set
+        {
+            _isGrounded = value;
+            animator.SetBool("isJumping", !_isGrounded);
+        }
+    }
     private bool canClimb;
-    private bool climbing;
-    private bool running;
+    private bool _isClimbing;
+    private bool isClimbing {
+         get
+        {
+            return _isClimbing;
+        }
+        set
+        {
+            _isClimbing = value;
+            animator.SetBool("isClimbing", _isClimbing);
+        }
+    }
+    private bool isRunning;
 
     public float moveSpeed = 3f;
     public float jumpStrength = 4f;
 
     private void Awake()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
+        // spriteRenderer = GetComponent<SpriteRenderer>();
         rigidbody = GetComponent<Rigidbody2D>();
         collider = GetComponent<Collider2D>();
+        animator = GetComponent<Animator>();
     }
 
-    private void OnEnable()
-    {
-        InvokeRepeating(nameof(AnimateSprite), 1f / 12f, 1f / 12f);
-    }
+    // private void OnEnable()
+    // {
+    //     InvokeRepeating(nameof(AnimateSprite), 1f / 12f, 1f / 12f);
+    // }
 
     private void OnDisable()
     {
@@ -42,11 +68,13 @@ public class Player : MonoBehaviour
     {
         CheckCollision();
         SetDirection();
+        animator.SetFloat("xVelocity", Math.Abs(direction.x));
+        animator.SetFloat("yVelocity", Math.Abs(direction.y));
     }
 
     private void CheckCollision()
     {
-        grounded = false;
+        isGrounded = false;
         canClimb = false;
 
         // the amount that two colliders can overlap
@@ -66,10 +94,10 @@ public class Player : MonoBehaviour
             if (hit.layer == LayerMask.NameToLayer("Ground"))
             {
                 // Only set as grounded if the platform is below the player
-                grounded = hit.transform.position.y < (transform.position.y - 0.5f + skinWidth);
+                isGrounded = hit.transform.position.y < (transform.position.y - 0.5f + skinWidth);
 
                 // Turn off collision on platforms the player is not grounded to
-                Physics2D.IgnoreCollision(overlaps[i], collider, !grounded);
+                Physics2D.IgnoreCollision(overlaps[i], collider, !isGrounded);
             }
             else if (hit.layer == LayerMask.NameToLayer("Ladder"))
             {
@@ -83,23 +111,24 @@ public class Player : MonoBehaviour
 
         if ((canClimb && Input.GetAxis("Vertical") > 0))
         {
-            climbing = true;
+            isClimbing = true;
         }
 
-        if (climbing)
+        if (isClimbing)
         {
             if (canClimb)
             {
+                direction.x = 0;
                 direction.y = Input.GetAxis("Vertical") * moveSpeed;
                 return;
             }
             else
             {
-                climbing = false;
+                isClimbing = false;
             }
         }
 
-        if (grounded && Input.GetButtonDown("Jump"))
+        if (isGrounded && Input.GetButtonDown("Jump"))
         {
             direction = Vector2.up * jumpStrength;
         }
@@ -108,13 +137,13 @@ public class Player : MonoBehaviour
             direction += Physics2D.gravity * Time.deltaTime;
         }
         // Prevent gravity from building up infinitely
-        if (grounded)
+        if (isGrounded)
         {
             direction.y = Mathf.Max(direction.y, -1f);
         }
 
         direction.x = Input.GetAxis("Horizontal") * moveSpeed;
-        running = direction.x > 0 || direction.x < 0;
+        isRunning = direction.x > 0 || direction.x < 0;
         if (direction.x > 0f)
         {
             transform.eulerAngles = Vector3.zero;
@@ -130,32 +159,32 @@ public class Player : MonoBehaviour
         rigidbody.MovePosition(rigidbody.position + direction * Time.fixedDeltaTime);
     }
 
-    private void AnimateSprite()
-    {
-        if (climbing)
-        {
-            spriteRenderer.sprite = climbSprite;
-        }
+    // private void AnimateSprite()
+    // {
+    //     if (climbing)
+    //     {
+    //         spriteRenderer.sprite = climbSprite;
+    //     }
 
-        if (running)
-        {
-            spriteIndex++;
+    //     if (running)
+    //     {
+    //         spriteIndex++;
 
-            if (spriteIndex >= runSprites.Length)
-            {
-                spriteIndex = 0;
-            }
+    //         if (spriteIndex >= runSprites.Length)
+    //         {
+    //             spriteIndex = 0;
+    //         }
 
-            if (spriteIndex > 0 && spriteIndex <= runSprites.Length)
-            {
-                spriteRenderer.sprite = runSprites[spriteIndex];
-            }
-        }
-        else if (grounded)
-        {
-            spriteRenderer.sprite = runSprites[0];
-        }
-    }
+    //         if (spriteIndex > 0 && spriteIndex <= runSprites.Length)
+    //         {
+    //             spriteRenderer.sprite = runSprites[spriteIndex];
+    //         }
+    //     }
+    //     else if (grounded)
+    //     {
+    //         spriteRenderer.sprite = runSprites[0];
+    //     }
+    // }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
